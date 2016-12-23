6969993691ee40c86c496298b252c0cecf6ae95f
Zhuojin Zou <zzjstudy@gmail.com>
2016-12-23 11:51:21 +0800

    simple client runner

--- src/github.com/cmu440/crunner/crunner.go
+++ src/github.com/cmu440/crunner/crunner.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"time"
 )
 
 const (
-	defaultHost = "localhost"
-	defaultPort = 9999
+	defaultHost  = "localhost"
+	defaultPort  = 9999
+	RECV_BUF_LEN = 1024
 )
 
 // To test your server implementation, you might find it helpful to implement a
@@ -15,5 +18,31 @@ const (
 // read and print out the server's echoed response to standard output. Whether or
 // not you add any code to this file will not affect your grade.
 func main() {
-	fmt.Println("Not implemented.")
+	conn, err := net.Dial("tcp", "127.0.0.1:" + fmt.Sprintf("%d", defaultPort))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+	buf := make([]byte, RECV_BUF_LEN)
+	for i := 0; i < 5; i++ {
+		//send message
+		msg := fmt.Sprintf("Hello World, %03d", i)
+		n, err := conn.Write([]byte(msg))
+		if err != nil {
+			println("Write Buffer Error:", err.Error())
+			break
+		}
+		fmt.Printf("client send: %s\n", msg)
+		
+		//receive message from server
+		n, err = conn.Read(buf)
+		if err != nil {
+			println("Read Buffer Error:", err.Error())
+			break
+		}
+		fmt.Printf("client receive: %s\n", string(buf[0:n]))
+		
+		//wait
+		time.Sleep(time.Second)
+	}
 }
